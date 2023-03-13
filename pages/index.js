559bfd83d6a76983703053091304b60c136b132c
Reuben Evans <reubene@gmail.com>
2023-03-13 14:55:40 +1300

    Fix text block

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,8 @@ export default function Home() {
             </div>
             <h2 className="eyebrow">Calls and Texts</h2>
             <h3>Siri lets you stay connected without lifting a finger.</h3>
-              <p>Siri can make calls or send texts for you whether you are driving, have your hands full, or are simply on the go.1 It can even announce your messages on your AirPods.2 It also offers proactive suggestions — like texting someone that you’re running late for a meeting — so you can stay in touch effortlessly.3</p>
+              <p>Siri can make calls or send texts for you whether you are driving, have your hands full, or are simply on the go. It can even announce your messages on your AirPods.
+                  It also offers proactive suggestions — like texting someone that you’re running late for a meeting — so you can stay in touch effortlessly.</p>
           </div>
           <Form />
       </main>
